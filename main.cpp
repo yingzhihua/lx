7d381469400d801d7674615c74dde38f8b6f7c92
yingzhihua <yingzh@flashdx.cn>
2019-09-09 14:40:57 +0800

    f

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFont>
+
+#include "module/sqlitemgr.h"
+#include "module/exglobal.h"
+#include "module/sequence.h"
+#include "module/log.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +14,24 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    QFont font;
+    font.setPixelSize(30);
+    app.setFont(font);
+
+    sqlitemgrinstance = new SqliteMgr();
+    sqlitemgrinstance->conn(Log::getDir()+"/data.db","sa","123456");
+    //init dao manager
+    //sqlitemgrinstance->init();
+    ExGlobal::exInit();
+
+    sequence = new Sequence();
+
+    if (argc > 1 && strcmp(argv[1],"debug")==0)
+        ExGlobal::setDebug(true);
+
+    qmlRegisterSingletonType<ExGlobal>("Dx.Global",1,0,"ExGlobal",exglobal_provider);
+    qmlRegisterSingletonType<Sequence>("Dx.Sequence",1,0,"Sequence",sequence_provider);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
